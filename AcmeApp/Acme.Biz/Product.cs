4569871af18bbb49e540d1b346ce01d843da2cdc
Matthew White <Matthew.White02@gmail.com>
2018-09-17 12:52:21 +0100

    Use a collection initialiser to create array

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -16,11 +16,14 @@ namespace Acme.Biz
         #region Constructors
         public Product()
         {
-            var colourOptions = new string[4];
-            colourOptions[0] = "Red";
-            colourOptions[1] = "Brown";
-            colourOptions[2] = "White";
-            colourOptions[3] = "Blue";
+            //var colourOptions = new string[4];
+            //colourOptions[0] = "Red";
+            //colourOptions[1] = "Brown";
+            //colourOptions[2] = "White";
+            //colourOptions[3] = "Blue";
+
+            string[] colourOptions = {"Red", "Brown", "White", "Blue"};
+
             Console.WriteLine(colourOptions);
         }
         public Product(int productId,
