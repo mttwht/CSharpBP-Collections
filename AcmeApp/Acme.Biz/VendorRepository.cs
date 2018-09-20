ebfff63d0e1b1ae83ec1326d2b79b3a35d936917
Unknown <Matthew.White02@gmail.com>
2018-09-20 15:05:38 +0100

    Retrieve an element from the list

--- AcmeApp/Acme.Biz/VendorRepository.cs
+++ AcmeApp/Acme.Biz/VendorRepository.cs
@@ -42,7 +42,7 @@ namespace Acme.Biz
                 vendors.Add(new Vendor() {VendorId=1, CompanyName="ABC Corp", Email="abc@abc.com"});
                 vendors.Add(new Vendor() {VendorId=2, CompanyName="XYZ Inc", Email="xyz@xyz.com"});
             }
-            Console.WriteLine(vendors);
+            Console.WriteLine(vendors[1]);
 
             return vendors;
         }
