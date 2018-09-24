fb1430b396484048ca4af0854cb931fb3418bc94
Unknown <Matthew.White02@gmail.com>
2018-09-24 13:41:04 +0100

    Update method to accept ICollection as parameter

--- AcmeApp/Tests/Acme.BizTests/VendorTests.cs
+++ AcmeApp/Tests/Acme.BizTests/VendorTests.cs
@@ -178,5 +178,23 @@ namespace Acme.Biz.Tests
             // Assert
             CollectionAssert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void SendEmailTestDictionary()
+        {
+            // Arrange
+            var vendorRepository = new VendorRepository();
+            var vendors = vendorRepository.RetrieveWithKeys();
+            var expected = new List<string>() {
+                "Message sent: Important message for: ABC Corp",
+                "Message sent: Important message for: XYZ Inc"
+            };
+
+            // Act
+            var actual = Vendor.SendEmail(vendors.Values, "Test message");
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
     }
 }
